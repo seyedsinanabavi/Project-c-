c624b0056f798d492483d1a115284b26bb7f84c8
DEXTER <125993415+seyedsinanabavi@users.noreply.github.com>
2023-11-24 12:19:46 -0800

    Add files via upload

---
+++ Hw4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+ float	x,y,s,m,a;
+int r;
+	cout<<"please enter your first number : ";
+	cin>>x;
+	cout<<"please enter your second number : ";
+	cin>>y;
+     s=x+y;
+     
+     cout<<s<<"  ";
+     
+     m=x-y;
+     cout<<m<<"  ";
+     a=x*y;
+     cout<<a;
+    
+     r=fmod(x,y);
+     cout<<r;
+    
+	
+	
+
+	
+}
